1a512f3887fdda0bc231b9e428c43e098e3c0b67
Karl McGuire <karl@karlmcguire.com>
2018-01-18 16:23:48 -0500

    delivery agent getting postback objects

--- delivery/delivery.go
+++ delivery/delivery.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	var (
+		db = redis.NewClient(&redis.Options{
+			Addr:     os.Getenv("REDIS_ADDR"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+		})
+		postback []string
+		err      error
+	)
+
+	println("watching redis at " + os.Getenv("REDIS_ADDR"))
+
+	for {
+		if postback, err = db.BLPop(0, "postbacks").Result(); err != nil {
+			// TODO: handle errors better
+			panic(err)
+		}
+
+		println(postback[1])
+
+		//go NewPostback(db, postback[1])
+		NewPostback(db, postback[1])
+	}
 }
--- delivery/postback.go
+++ delivery/postback.go
@@ -6,6 +6,8 @@ import (
 	"net/url"
 	"regexp"
 	"strings"
+
+	"github.com/go-redis/redis"
 )
 
 var (
@@ -19,8 +21,68 @@ type Postback struct {
 	Params map[string]string `json:"-"`
 }
 
-func (p *Postback) UnmarshalJSON(value []byte) error {
-	return json.Unmarshal(value, p)
+func NewPostback(db *redis.Client, key string) {
+	var (
+		postback *Postback = &Postback{}
+		value    []byte
+		err      error
+	)
+
+	// get the json object from the postback:[uuid] key in redis
+	if value, err = db.Get(key).Bytes(); err != nil {
+		panic(err)
+	}
+
+	// delete the postback:[uuid] key from redis
+	if err = db.Del(key).Err(); err != nil {
+		panic(err)
+	}
+
+	// unmarshal the json object into postback variable
+	if err = postback.UnmarshalJSON(value); err != nil {
+		panic(err)
+	}
+
+	// parse the postback url for params
+	if err = postback.Parse(nil); err != nil {
+		panic(err)
+	}
+
+	// start listening for data objects on postback:[uuid]:data
+	postback.Listen(db, key+":data")
+}
+
+func (p *Postback) Listen(db *redis.Client, key string) {
+	var (
+		data []string
+		err  error
+	)
+
+	// the count field added by the ingestion agent allows this goroutine to
+	// only live for as long as there are still data objects to receive
+	for i := 0; i < p.Count; i++ {
+		// block until data object is available
+		if data, err = db.BLPop(0, key).Result(); err != nil {
+			panic(err)
+		}
+
+		// handle the data object
+		//go p.Respond(data[1])
+		p.Respond(data[1])
+	}
+}
+
+func (p *Postback) Respond(value string) {
+	var (
+		params map[string]string
+		err    error
+	)
+
+	if err = json.Unmarshal([]byte(value), &params); err != nil {
+		panic(err)
+	}
+
+	println(p.Fill(params))
 }
 
 func (p *Postback) Parse(defaults map[string]string) error {
@@ -52,16 +114,20 @@ func (p *Postback) Fill(values map[string]string) string {
 
 	// for every param defined in p.Url
 	for param, paramDefault := range p.Params {
-		// if the values map contains a definition for the param, then put it in
-		// the new url
 		if value, exists := values[param[1:len(param)-1]]; exists {
+			// the values map contains a definition for the param, so put it in
+			// the new url
 			filled = strings.Replace(filled, param, url.QueryEscape(value), -1)
-			// if the values map doesn't contain a definition for the param, then
-			// put the default value in the new url
 		} else {
+			// the values map doesn't contain a definition for the param, so put
+			// the default value in the new url
 			filled = strings.Replace(filled, param, url.QueryEscape(paramDefault), -1)
 		}
 	}
 
 	return filled
+}
+
+func (p *Postback) UnmarshalJSON(value []byte) error {
+	return json.Unmarshal(value, p)
 }
